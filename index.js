8822fd2105e4108873c991c72a8a04ff6399296b
Robert <49911127+rfreeman203@users.noreply.github.com>
2021-11-22 10:09:28 -0500

    created question list in form of radio buttons

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+let questionList = require('./questions.json')
 
 const app = express();
 
 app.use(express.static('static'));
+
+app.get('/questionsInJson', function(req,res){
+    res.json(questionList); 
+})
 
 app.listen(80);
