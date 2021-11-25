0073942cd57adf49dd825b1351636703b108c2a5
Robert <49911127+rfreeman203@users.noreply.github.com>
2021-11-25 15:38:49 -0500

    Updated so answers are never available client side

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
-let questionList = require('./questions.json')
+const questionList = require('./questions.json');
 
 const app = express();
 
 app.use(express.static('static'));
 
 app.get('/questionsInJson', function(req,res){
-    res.json(questionList); 
+    let questionsNoAnswers = JSON.parse(JSON.stringify(questionList));
+    
+    for(i in questionsNoAnswers){
+        delete questionsNoAnswers[i].answerIndex;
+    }
+    
+    res.json(questionsNoAnswers); 
 })
+
+
+app.get('/answersInJson', function(req,res){
+
+    let qIndex = req.query.q;
+    let aIndex = req.query.a;
+    let feedback = "";
+
+    let question = questionList[qIndex];
+
+    if(question.answerIndex == aIndex){
+        feedback = "Correct! " + qIndex;
+    }
+    else{
+        feedback = "Incorrect! " + qIndex;
+    }
+
+    res.send(feedback);
+})
+
+
 
 app.listen(80);
